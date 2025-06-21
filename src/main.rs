b36a9e63fe05daccb239202944e5f924dc2faaaa
Lanota81L <1926974795@qq.com>
2025-06-21 12:49:28 +0800

    ProBase

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use gtk::{prelude::*, ApplicationWindow};
+use gtk::{glib, Application};
+
+const APP_ID: &str = "org.gtk_rs.Sudoku";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Sudoku")
+        .build();
+    window.present();
+}
